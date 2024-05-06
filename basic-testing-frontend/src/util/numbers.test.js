2f2f2911641cba28d2554aeb0994fb3cbb260d0e
Luke <lrsorrenti@hotmail.com>
2024-05-06 07:31:01 -0500

    added initial example of integration test

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,23 +1,33 @@
-import { expect, test } from "vitest";
-import { transformToNumber } from "./numbers";
+import { describe, expect, test } from "vitest";
+import { cleanNumbers, transformToNumber } from "./numbers";
 
-test("should receive a number in a format '10' and return the number value of the string", () => {
-    // Arrange
-    const value = '10';
-    const expectedResult = 10;
-    // Act
-    const result = transformToNumber(value);
-    // Assert
-    expect(result).toBe(expectedResult);
-    // Or 
-    // expect(result).toBeTypeOf('number');
+describe('transformToNumber()', () => {
+    test("should receive a number in a format '10' and return the number value of the string", () => {
+        // Arrange
+        const value = '10';
+        const expectedResult = 10;
+        // Act
+        const result = transformToNumber(value);
+        // Assert
+        expect(result).toBe(expectedResult);
+        // Or 
+        // expect(result).toBeTypeOf('number');
+    });
+    
+    test('should return NaN if an invalid string is provides', () => {
+        // Arrange 
+        const value = 'ten';
+        // Act
+        const result = transformToNumber(value);
+        // Assert
+        expect(result).toBeNaN();
+    });
 });
 
-test('should return NaN if an invalid string is provides', () => {
-    // Arrange 
-    const value = 'ten';
-    // Act
-    const result = transformToNumber(value);
-    // Assert
-    expect(result).toBeNaN();
+describe('cleanNumbers()', () => {
+    test('should return an array if number values if an array of stirng number values is provided')
+    const numberValues = ['1', '2', '3'];
+
+    const cleanedNumbers = cleanNumbers(numberValues);
+    expect(cleanedNumbers[0]).toBeTypeOf('number');
 });
